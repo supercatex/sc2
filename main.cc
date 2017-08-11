c3df9ed57dba9247c427881c40a68d005eaaf24b
Kinda <lamkinun@gmail.com>
2017-08-12 01:56:29 +0800

    主程序

--- main.cc
+++ main.cc
@@ -1,10 +1,8 @@
 #include <sc2api\sc2_api.h>
 #include <iostream>
+#include "CustomBot.cc"
 
 using namespace sc2;
-
-//自訂義Bot
-class CustomBot : public Agent {};
 
 //主程序
 int main(int argc, char* argv[]) {
