2577da530f5f76a2fd6c32e098ab614968a74137
Kinda <lamkinun@gmail.com>
2017-08-12 01:17:14 +0800

    Initial commit

---
+++ main.cc
@@ -0,0 +1,37 @@
+#include <sc2api\sc2_api.h>
+#include <iostream>
+
+using namespace sc2;
+
+class CustomBot : public Agent {};
+
+int main(int argc, char* argv[]) {
+
+    //遊戲協調員
+    Coordinator coordinator;
+    coordinator.LoadSettings(argc, argv);
+
+    //true: 正常速度, false: 超快進程.
+    coordinator.SetRealtime(true);
+    //遊戲視窗起始位置.
+    coordinator.SetWindowLocation(450, 200);
+
+    //自訂義Bot.
+    CustomBot bot;
+    //設置參與玩家
+    coordinator.SetParticipants({
+        //Terran: 人族, Zerg: 蟲族, Protoss: 神族, Random: 隨機.
+        CreateParticipant(Race::Random, &bot),
+        CreateComputer(Race::Random, Difficulty::HardVeryHard)
+    });
+
+    //啟動遊戲主程序
+    coordinator.LaunchStarcraft();
+    //設置地圖並開始遊戲
+    coordinator.StartGame(sc2::kMapBelShirVestigeLE);
+
+    //遊戲迴圈
+    while (coordinator.Update()) {}
+
+    return 0;
+}
